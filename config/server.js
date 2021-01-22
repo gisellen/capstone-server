c466fdf5b6d4029717248e15024136765a4b9531
Gisellen <giselle.domingo.x@gmail.com>
2021-01-22 18:10:52 -0500

    Initial commit

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', 'b344a7be136298b0477be567d3df5f48'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || '9417e73c-c314-4587-83f4-8741fd844384'
+};
